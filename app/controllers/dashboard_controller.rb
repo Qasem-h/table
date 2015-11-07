71e87eddc90a64aeb3319f3ad127ab2df754a99e
Artem Biserov <artem.biserov@gmail.com>
2015-11-07 15:15:44 +0300

    remove unused helpers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,4 @@
 class DashboardController < ApplicationController
-  include TablesHelper
-
   def index
     country = params[:country] || 'england'
     @leagues = League.where(country: country).joins(:matches).includes(matches: [odd_types: :bookmaker])
--- app/helpers/tables_helper.rb
+++
@@ -1,9 +0,0 @@
-module TablesHelper
-  module_function
-
-  def parse
-    f = File.open("public/odds/england.xml")
-    odds_feed = OddsFeedParser.parse(f)
-    odds_feed.matches.select { |match| match.type.present? }
-  end
-end
